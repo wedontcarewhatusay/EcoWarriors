bd293d88ebc129e924caebc0bebdaf058a118d64
nightwatch92 <nightwatch92@gmail.com>
2021-02-27 22:52:20 +0200

    lib/screens/ml.dart - Firebase label integration.

---
+++ lib/screens/ml.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:io';
+
+
+import 'package:cameraapp/screens/preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:uuid/uuid.dart';
+
+class Label {
+  //
+  // String imagePath;
+  // set imgPath(String imagePath) {
+  //   this.imagePath = imagePath;
+  // }
+
+  Future<void> detectLabels(String imagePath) async {
+    print("this is image path detectLabels ${imagePath}");
+    final FirebaseVisionImage visionImage =
+    FirebaseVisionImage.fromFilePath(imagePath);
+    final ImageLabeler labelDetector = FirebaseVision.instance.imageLabeler();
+
+    final List<ImageLabel> labels =
+    await labelDetector.processImage(visionImage);
+
+    List<String> labelTexts = new List();
+    for (ImageLabel label in labels) {
+      final String text = label.text;
+
+      labelTexts.add(text);
+    }
+
+    final String uuid = Uuid().v1();
+    final String downloadURL = await _uploadFile(uuid, imagePath) ;
+
+    await _addItem(downloadURL, labelTexts);
+  }
+    Future<void> _addItem(String downloadURL, List<String> labels) async {
+    await FirebaseFirestore.instance
+        .collection('items')
+        .add(<String, dynamic>{'downloadURL': downloadURL, 'labels': labels});
+  }
+
+  Future<String> _uploadFile(filename, imagePath) async {
+    final File file = File(imagePath);
+    print("this is image path detectLabels ${imagePath}");
+
+    final firebase_storage.Reference ref =
+        firebase_storage.FirebaseStorage.instance.ref().child('$filename.jpg');
+    final firebase_storage.UploadTask uploadTask = ref.putFile(
+      file,
+      firebase_storage.SettableMetadata(
+        contentLanguage: 'en',
+      ),
+    );
+
+    firebase_storage.TaskSnapshot snapshot = await uploadTask;
+    final downloadURL = await snapshot.ref.getDownloadURL();
+    return downloadURL;
+  }
+
+
+}
