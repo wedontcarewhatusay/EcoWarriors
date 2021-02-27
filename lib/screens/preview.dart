4603f15897d0b49b4268d4141d1a560898490af5
nightwatch92 <nightwatch92@gmail.com>
2021-02-27 23:37:26 +0200

    Visualize all the available items.

--- lib/screens/preview.dart
+++ lib/screens/preview.dart
@@ -4,61 +4,103 @@ import 'dart:typed_data';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class PreviewScreen extends StatefulWidget {
-  final String imgPath;
-  final String fileName;
-  PreviewScreen({this.imgPath, this.fileName});
 
-  @override
-  _PreviewScreenState createState() => _PreviewScreenState();
-}
+import 'package:cameraapp/screens/camera.dart';
 
-class _PreviewScreenState extends State<PreviewScreen> {
+
+class ItemsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
+        title: const Text('My Items'),
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Image.file(File(widget.imgPath),fit: BoxFit.cover,),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: double.infinity,
-                height: 60,
-                color: Colors.black,
-                child: Center(
-                  child: IconButton(
-                    icon: Icon(Icons.share,color: Colors.white,),
-                    onPressed: (){
-                      getBytes().then((bytes) {
-                        print('here now');
-                        print(widget.imgPath);
-                        print(bytes.buffer.asUint8List());
-                        Share.file('Share via', widget.fileName, bytes.buffer.asUint8List(), 'image/path');
-                      });
-                    },
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      )
-    );
+      body: ItemsList(firestore: FirebaseFirestore.instance),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => CameraScreen()),
+                      );
+        },
+child: const Icon(Icons.add),
+),
+      );
   }
+}
 
-  Future getBytes () async {
-    Uint8List bytes = File(widget.imgPath).readAsBytesSync() as Uint8List;
-//    print(ByteData.view(buffer))
-    return ByteData.view(bytes.buffer);
-  }
+class ItemsList extends StatelessWidget {
+        ItemsList({this.firestore});
+
+        final FirebaseFirestore firestore;
+
+        @override
+                Widget build(BuildContext context) {
+                        return StreamBuilder<QuerySnapshot>(
+                                        stream: firestore.collection('items').snapshots(),
+                                        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                                        if (!snapshot.hasData) return const Text('Loading...');
+                                        final int itemsCount = snapshot.data.docs.length;
+                                        return ListView.builder(
+                                                        itemCount: itemsCount,
+                                                        itemBuilder: (_, int index) {
+                                                        final DocumentSnapshot document = snapshot.data.docs[index];
+                                                        return SafeArea(
+                                                                        top: false,
+                                                                        bottom: false,
+                                                                        child: Container(
+                                                                                padding: const EdgeInsets.all(8.0),
+                                                                                height: 310.0,
+                                                                                child: Card(
+                                                                                        shape: RoundedRectangleBorder(
+                                                                                                borderRadius: BorderRadius.only(
+                                                                                                        topLeft: Radius.circular(16.0),
+                                                                                                        topRight: Radius.circular(16.0),
+                                                                                                        bottomLeft: Radius.circular(16.0),
+                                                                                                        bottomRight: Radius.circular(16.0),
+                                                                                                        ),
+                                                                                                ),
+                                                                                        child: Column(
+                                                                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                                                                children: <Widget>[
+                                                                                                // photo and title
+                                                                                                SizedBox(
+                                                                                                        height: 184.0,
+                                                                                                        child: Stack(
+                                                                                                                children: <Widget>[
+                                                                                                                Positioned.fill(
+                                                                                                                        child: Image.network(document['downloadURL']),
+                                                                                                                        ),
+                                                                                                                ],
+                                                                                                                ),
+                                                                                                        ),
+                                                                                                Expanded(
+                                                                                                        child: Padding(
+                                                                                                                padding:
+                                                                                                                const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                                                                                                                child: DefaultTextStyle(
+                                                                                                                        softWrap: true,
+                                                                                                                        //overflow: TextOverflow.,
+                                                                                                                        style: Theme.of(context).textTheme.subhead,
+                                                                                                                        child: Column(
+                                                                                                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                                                                                                children: <Widget>[
+                                                                                                                                Text(document['labels'].join(', ')),
+                                                                                                                                ]),
+                                                                                                                        ),
+                                                                                                                ),
+                                                                                                        ),
+                                                                                                ],
+                                                                                                ),
+                                                                                                ),
+                                                                                                ),
+                                                                                                );
+                                                        },
+                                                        );
+                                        },
+                                        );
+                }
 }
